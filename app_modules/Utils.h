e8368662d567a2fe8bc11a7f52434e6db876746b
AnkIt1202 <as547925@gmail.com>
2023-02-21 20:08:57 -0800

    Update Utils.h

--- app_modules/Utils.h
+++ app_modules/Utils.h
@@ -22,27 +22,49 @@ namespace AppUtils {
         public:
 
         static Command findCommand(const map<Command, bool> &values) {
+
             Command result;
             int trueCount = 0;
 
             for (const auto &[command, value]: values) {
+
                 if (value) {
+
                     trueCount++;
                     result = command;
+
                 }
             }
         }
 
         static string joinVectorToString(vector<string> tokens, int fromIndex = 0, string separator = " ") {
+
             ostringstream result;
             for (vector<string>::iterator it = tokens.begin() + fromIndex; it != tokens.end(); it++) {
+
                 if (it != tokens.begin() + fromIndex) {
                     result << separator;
                 }
                 result << *it;
             }
             return result.str();
-        };        
+        };
+
+
+        static string trimAfterRootDir(string dir_name, string path) {
+
+            vector<string> parts = Utils::split(path, '/');
+            auto it = find(parts.begin(), parts.end(), dir_name);       
+
+            if (it != parts.end()) {
+                parts.erase(it + 1, parts.end());
+            }
+
+            string final_path = Utils::joinVectorToString(parts, 0, "/");
+            return final_path;
+        }
+        
+         
 
     };
 }
