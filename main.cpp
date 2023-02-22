8b63e6e15ca89afcc6b9efeccae64456f9952e92
Madhvik-P <msp10@sfu.ca>
2023-02-22 14:58:28 -0800

    Updated main

--- main.cpp
+++ main.cpp
@@ -8,5 +8,34 @@ using namespace AppConstants::DefaultDirectory;
 using namespace std;
 
 int main(int argc, char* argv[]) {
+     
+     if(argc != 2) {
+        cout << "\nPlease provide the keyfile for authentication purposes.\n" << endl;
+        return 0;
+    }
 
+    Auth auth;
+
+    string username = auth.getUsernameFromPrivateKey(argv[1]);
+    if(username.empty()) return 0;
+
+    bool is_admin = username == ADMIN;
+
+    int status = auth.checkFileSystemExits(is_admin);
+    if(status == 404 && !is_admin) {
+        cout << "\nIt appears that you are a new user on this application. To get started, you will need to create an administrator account first.\n" << endl;
+        return 0;
+    }
+
+    int create_admin_status = auth.checkAndCreateAdmin(is_admin);
+
+    if (create_admin_status != 201) {
+        int login_status = auth.login(username);
+        if (login_status == 0) return 0;
+    }
+    auth.changeDirToUsernameIfNotAdmin(is_admin, username);
+
+    Menu menu(username, is_admin);
+    menu.showCMD();
+    return 0;
 }
