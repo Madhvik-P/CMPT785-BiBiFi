410a7aa58c0444a5d2a23a6ea12d381c4450dfad
AnkIt1202 <as547925@gmail.com>
2023-02-25 16:26:07 -0800

    Create KeyEncrypter.h

---
+++ app_modules/KeyEncrypter.h
@@ -0,0 +1,24 @@
+#ifndef FILESYSTEM_APP_KEYENCRYPTER_H
+#define FILESYSTEM_APP_KEYENCRYPTER_H
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/rand.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "AppConstants.h"
+#include "FileEncrypter.h"
+
+using namespace std;
+using namespace AppConstants::DefaultDirectory;
+
+class KeyEncrypter {
+
+public: 
+
+
+
+};
+
+#endif //FILESYSTEM_APP_KEYENCRYPTER_H
