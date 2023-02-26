fa7554d820169718a54cf2d2a9ff6a778ec80493
AnkIt1202 <as547925@gmail.com>
2023-02-25 18:08:14 -0800

    update key-encrypter.h

--- app_modules/KeyEncrypter.h
+++ app_modules/KeyEncrypter.h
@@ -17,6 +17,30 @@ class KeyEncrypter {
 
 public: 
 
+    int generateAndStoreAESKey(string username, vector<string> paths){
+
+        // Generate AES key
+        const int AES_KEY_SIZE = 256;
+        unsigned char aes_key[AES_KEY_SIZE / 8];
+        if (!RAND_bytes(aes_key, AES_KEY_SIZE / 8)) {
+            cout << "Error generating AES key" << endl;
+            return 1;
+        }
+
+        // Load public key
+        RSA *public_key = nullptr;
+        string public_key_name = paths[0] + "/" + username+ PUBLIC_KEY_EXT;
+        char* public_key_file = new char[public_key_name.length() + 1];
+        strcpy(public_key_file, public_key_name.c_str());
+        FILE *public_key_fp = fopen(public_key_file, "r");
+
+        
+
+
+
+    }
+
+
 
 
 };
