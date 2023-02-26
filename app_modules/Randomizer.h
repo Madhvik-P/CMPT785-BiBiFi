704677f4fffeb75b923a40b2134d13ee266c707b
AnkIt1202 <as547925@gmail.com>
2023-02-25 19:04:52 -0800

    Update Randomizer.h

--- app_modules/Randomizer.h
+++ app_modules/Randomizer.h
@@ -16,6 +16,34 @@ class Randomizer {
 
 public:
 
+
+    static string getTranslatedPathRev(string root_dir, string input) {
+        map<string, string> key_value_pairs = getDataFromMetaFile(root_dir);
+        vector<string> dirs;
+        istringstream iss(input);
+        string dir;
+        while (getline(iss, dir, '/')) {
+            dirs.push_back(dir);
+        }
+        // Traverse the directories and replace the value with its key
+        for (int i = 0; i < dirs.size(); i++) {
+            for (auto const& [key, value] : key_value_pairs) {
+                if (dirs[i] == value) {
+                    dirs[i] = key;
+                    break;
+                }
+            }
+        }
+        string output;
+        for (int i = 0; i < dirs.size(); i++) {
+            output += dirs[i];
+            if (i != dirs.size() - 1) {
+                output += "/";
+            }
+        }
+        return output;
+    }
+
 };
 
 #endif //FILESYSTEM_APP_RANDOMIZER_H
