a3785031f32ffb1c02a8537a20c0a39c6ceb05c3
AnkIt1202 <as547925@gmail.com>
2023-02-25 18:47:00 -0800

    Update Utils.h

--- app_modules/Utils.h
+++ app_modules/Utils.h
@@ -35,13 +35,59 @@ namespace AppUtils {
             return (trueCount == 1) ? result : CMD_INVALID;
         }
 
+        static string joinVectorToString(vector<string> tokens, int fromIndex = 0, string separator = " ") {
 
+            ostringstream result;
+            for (vector<string>::iterator it = tokens.begin() + fromIndex; it != tokens.end(); it++) {
+                if (it != tokens.begin() + fromIndex) {
+                    result << separator;
+                }
+                result << *it;
+            }
+            return result.str();
+        };
 
-        
+        static string trimAfterRootDir(string dir_name, string path) {
+            vector<string> parts = Utils::split(path, '/');
+            auto it = find(parts.begin(), parts.end(), dir_name);
+            if (it != parts.end()) {
+                parts.erase(it + 1, parts.end());
+            }
+            string final_path = Utils::joinVectorToString(parts, 0, "/");
+            return final_path;
+        }
+
+        static string getRootDirPath(string pwd_path, string dir_name){
+
+            string root_dir_path = Utils::trimAfterRootDir(dir_name,pwd_path);
+            return  root_dir_path;
+
+        }
+
+        static vector<string> getPublicAndPrivateKeysPath(string root_path){
+
+            vector<string> paths;
+
+            vector<string> data = split(root_path, '/');
+            data.pop_back();
+
+            string private_key_path = joinVectorToString(data, 0, "/");
+            string public_key_path = root_path + "/" + Randomizer::getMetaValue(root_path, USERS);
+
+            paths.push_back(public_key_path);
+            paths.push_back(private_key_path);
+            return paths;
+
+        }
+
+        static string getPublicUserKeys() {
+            string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
+            string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
+            vector<string> paths = Utils::getPublicAndPrivateKeysPath(root_path);
+            return paths[0];
+        }
 
     };
 }
-
-
 
 #endif //FILESYSTEM_APP_UTILS_H
