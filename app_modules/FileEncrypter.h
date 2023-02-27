1c173dd164ed622480a5a0e6bda49af8fb7a4f03
Anant Awasthy <anant.awasthy29@gmail.com>
2023-02-26 18:29:44 -0800

    refactored the redundant code and oraganized the project structure (#22)

--- app_modules/FileEncrypter.h
+++ app_modules/FileEncrypter.h
@@ -11,14 +11,14 @@
 
 using namespace std;
 
-class FileEncryptor {
+class FileEncrypter {
 
 private:
     static unsigned char key[16];
     KeyEncrypter mKeyEncrypter;
 
 public:
-
+    // 
     static void encrypt_file(string file_name, string contents) {
         AES_KEY aes_key;
         AES_set_encrypt_key(key, 128, &aes_key);
@@ -48,6 +48,7 @@ public:
         out_file.close();
     }
 
+    // 
     static vector<unsigned char> decrypt_file(string file_name, string actual_filename = "") {
         AES_KEY aes_key;
         AES_set_decrypt_key(key, 128, &aes_key);
@@ -80,52 +81,60 @@ public:
         out_buf.resize(out_buf.size() - padding);
         return out_buf;
     }
-
+    // 
     static void show_decrypted_data(string file_name, string actual_filename) {
         vector<unsigned char> out_buf = decrypt_file(file_name, actual_filename);
         cout.write((char*)out_buf.data(), out_buf.size());
         cout << endl;
     }
-
+    // 
     static string get_decrypted_data(string file_name) {
         vector<unsigned char> out_buf = decrypt_file(file_name);
         return string((char*)out_buf.data(), out_buf.size());
     }
 
-
-    string print_decrypted_file(string private_key_str, string file_path) {
-        OpenSSL_add_all_algorithms();
-        ERR_load_crypto_strings();
-
-        // Convert private key string to RSA structure
-        const char* private_key_cstr = private_key_str.c_str();
-        BIO* bio = BIO_new_mem_buf((void*)private_key_cstr, -1);
-        RSA* rsa = PEM_read_bio_RSAPrivateKey(bio, NULL, NULL, NULL);
-        BIO_free(bio);
-
-        // Read encrypted data from file
-        ifstream encryptedDataFile(file_path, ios::in | ios::binary);
-        if (!encryptedDataFile) {
-            cout << "print_decrypted_file: Error opening encrypted data file." << endl;
-            RSA_free(rsa);
-            return "";
-        }
-        vector<unsigned char> encryptedData((istreambuf_iterator<char>(encryptedDataFile)), istreambuf_iterator<char>());
-        encryptedDataFile.close();
-
-        // Decrypt encrypted data
-        vector<unsigned char> decryptedData(RSA_size(rsa));
-        int decryptedDataLen = RSA_private_decrypt(encryptedData.size(), &encryptedData[0], &decryptedData[0], rsa, RSA_PKCS1_OAEP_PADDING);
-        if (decryptedDataLen == -1) {
-            cout << "Error decrypting data." << endl;
-            RSA_free(rsa);
-            return "";
-        }
-
-        RSA_free(rsa);
-        return string((char*)&decryptedData[0], decryptedDataLen);
+    void show_decrypted_data_dyn(string file_name, string actual_filename, vector<string> paths) {
+        vector<unsigned char> out_buf = decrypt_file_dyn(file_name, actual_filename, paths);
+        cout.write((char*)out_buf.data(), out_buf.size());
+        cout << endl;
     }
 
+    string get_decrypted_data_dyn(string file_name, string actual_filename, vector<string> paths) {
+        vector<unsigned char> out_buf = decrypt_file_dyn(file_name, actual_filename, paths);
+        return string((char*)out_buf.data(), out_buf.size());
+    }
+
+    void encrypt_file_dyn(string file_name, string contents, vector<string> paths, string username) {
+        AES_KEY aes_key;
+        AES_set_encrypt_key(mKeyEncrypter.decryptAESKey(paths,ADMIN), 128, &aes_key);
+
+        vector<unsigned char> in_buf(contents.begin(), contents.end());
+        int padding = 16 - (in_buf.size() % 16);
+        for (int i = 0; i < padding; i++) {
+            in_buf.push_back('\0');
+        }
+        vector<unsigned char> out_buf;
+        for (int i = 0; i < in_buf.size(); i += 16) {
+            unsigned char block_in[16], block_out[16];
+            for (int j = 0; j < 16; j++) {
+                block_in[j] = in_buf[i + j];
+            }
+            AES_encrypt(block_in, block_out, &aes_key);
+            for (int j = 0; j < 16; j++) {
+                out_buf.push_back(block_out[j]);
+            }
+        }
+        ofstream out_file(file_name, ios::out | ios::binary);
+        if (!out_file.is_open()) {
+            cout << "Error: Unable to create file " << file_name << endl;
+            return;
+        }
+        out_file.write((char*)out_buf.data(), out_buf.size());
+        out_file.close();
+    }
+
+
+    // 
     vector<unsigned char> decrypt_file_dyn(string file_name, string actual_filename, vector<string> paths) {
         AES_KEY aes_key;
         AES_set_decrypt_key(mKeyEncrypter.decryptAESKey(paths,ADMIN), 128, &aes_key);
@@ -158,8 +167,7 @@ public:
         out_buf.resize(out_buf.size() - padding);
         return out_buf;
     }
-
 };
 
-unsigned char FileEncryptor::key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+unsigned char FileEncrypter::key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 #endif //FILESYSTEM_APP_FILEENCRYPTOR_H
--- app_modules/Menu.h
+++ app_modules/Menu.h
@@ -44,143 +44,144 @@ private:
     FileEncrypter mFileEncrypter;
 
 public:
-
+    // 
     Menu(string u, bool a) {
         username = u;
         is_admin = a;
     };
-
+    // 
     int showCMD() {
         // TODO: need to get the value from the main function
         displayMenu();
 
-    while (true) {
-        string input;
-        implPWD();
+        while (true) {
+            string input;
+            implPWD();
 
-        if(current_dir_name == FILE_SYSTEM) cout << "/ $ ";
-        else cout << current_dir_name + " $ ";
+            if(current_dir_name == FILE_SYSTEM) cout << "/ $ ";
+            else cout << current_dir_name + " $ ";
 
-        getline(cin, input);
+            getline(cin, input);
 
-        vector<string> tokens = Utils::split(input, ' ');
+            vector<string> tokens = Utils::split(input, ' ');
 
-        Command selectedCommand = Utils::getCommandName(tokens, input);
+            Command selectedCommand = Utils::getCommandName(tokens, input);
 
-        if(selectedCommand == CMD_EXIT) {
-            break;
-        }
-
-        switch (selectedCommand) {
-            case CMD_ADDUSER: {
-                if (is_admin) {
-                    string username_to_be_added = tokens[1];
-                    string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
-                    vector<string> paths = Utils::getPublicAndPrivateKeysPath(root_path);
-                    int status = createUser(root_path, username_to_be_added);
-                    if(status == 201) {
-                        mCreateUser.generateKeysForUser(username_to_be_added, paths, username);
-                        mKeyEncrypter.generateAndStoreAESKey(username_to_be_added, paths);
-                    }
-                }else {
-                    cout << "You are not admin user. Please contact admit to create new user." << endl;
-                }
+            if(selectedCommand == CMD_EXIT) {
                 break;
             }
 
-            case CMD_MKFILE: {
-                if(Utils::startsWithPersonal(getRelativePWDPath())) {
-                    string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
-                    string _username = Utils::getUsernameFromFilePath(Utils::getPwdPath());
-                    string target_username = Randomizer::getMetaKey(root_path,_username);
-                    bool is_admin_inside_users_dir = Randomizer::getMetaValue(root_path, USER + target_username).empty() && is_admin;
+            switch (selectedCommand) {
+                case CMD_ADDUSER: {
+                    if (is_admin) {
+                        string username_to_be_added = tokens[1];
+                        string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
+                        vector<string> paths = Utils::getPublicAndPrivateKeysPath(root_path);
+                        int status = createUser(root_path, username_to_be_added);
+                        if(status == 201) {
+                            mCreateUser.generateKeysForUser(username_to_be_added, paths, username);
+                            mKeyEncrypter.generateAndStoreAESKey(username_to_be_added, paths);
+                        }
+                    }else {
+                        cout << "You are not admin user. Please contact admit to create new user." << endl;
+                    }
+                    break;
+                }
+
+                case CMD_MKFILE: {
+                    if(Utils::startsWithPersonal(getRelativePWDPath())) {
+                        string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
+                        string _username = Utils::getUsernameFromFilePath(Utils::getPwdPath());
+                        string target_username = Randomizer::getMetaKey(root_path,_username);
+                        bool is_admin_inside_users_dir = Randomizer::getMetaValue(root_path, USER + target_username).empty() && is_admin;
 //                    if(is_admin_inside_users_dir){
 //                        cout << "\nAs an administrator, you are only authorized to access and read files on a system or server. You do not have the permission to modify or create files on the system.\n" << endl;
 //                        break;
 //                    }
-                    implMKFILE(tokens[1], Utils::joinVectorToString(tokens, 2, " "));
-                }
-                break;
-            }
-
-            case CMD_MKDIR: {
-                if(Utils::startsWithPersonal(getRelativePWDPath())) {
-                    string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
-                    string _username = Utils::getUsernameFromFilePath(Utils::getPwdPath());
-
-                    string filename_value = Utils::translateDirOrFileWhenCreated(tokens[1]);
-                    bool isPresent = Utils::checkDirectoryExists(filename_value);
-                    if (!isPresent) mkdir(filename_value.c_str(), 0777);
-                    else cout << "Directory already exists" << endl;
-                }
-                break;
-            }
-
-            case CMD_SHARE: {
-                if (username == tokens[2]) {
-                    cout << "The operation cannot be performed as you are attempting to share a file with your own account." << endl;
+                        implMKFILE(tokens[1], Utils::joinVectorToString(tokens, 2, " "));
+                    }
                     break;
                 }
 
-                bool isReceiverAdmin = ADMIN == tokens[2];
-                bool isFilePresent = checkFilePresentInCurrentDir(tokens[1]);
-                if(!isFilePresent) break;
-                bool isUser = isReceiverAdmin || checkUsernameExist(tokens[2]);
-                bool isPresent = isFilePresent && isUser;
+                case CMD_MKDIR: {
+                    if(Utils::startsWithPersonal(getRelativePWDPath())) {
+                        string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
+                        string _username = Utils::getUsernameFromFilePath(Utils::getPwdPath());
 
-                if(isPresent) {
+                        string filename_value = Utils::translateDirOrFileWhenCreated(tokens[1]);
+                        bool isPresent = Utils::checkDirectoryExists(filename_value);
+                        if (!isPresent) mkdir(filename_value.c_str(), 0777);
+                        else cout << "Directory already exists" << endl;
+                    }
+                    break;
+                }
+
+                case CMD_SHARE: {
+                    if (username == tokens[2]) {
+                        cout << "The operation cannot be performed as you are attempting to share a file with your own account." << endl;
+                        break;
+                    }
+
+                    bool isReceiverAdmin = ADMIN == tokens[2];
+                    bool isFilePresent = checkFilePresentInCurrentDir(tokens[1]);
+                    if(!isFilePresent) break;
+                    bool isUser = isReceiverAdmin || checkUsernameExist(tokens[2]);
+                    bool isPresent = isFilePresent && isUser;
+
+                    if(isPresent) {
+                        string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
+                        string  filename = Randomizer::getMetaValue(root_path,tokens[1]);
+                        string share_username = Randomizer::getMetaValue(root_path,tokens[2]);
+                        mShareFile.implShare(filename, share_username, tokens[1], tokens[2], username);
+                    }
+                    break;
+                }
+
+                case CMD_CAT: {
                     string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
-                    string  filename = Randomizer::getMetaValue(root_path,tokens[1]);
-                    string share_username = Randomizer::getMetaValue(root_path,tokens[2]);
-                    mShareFile.implShare(filename, share_username, tokens[1], tokens[2], username);
+                    string translated_filename = Randomizer::getMetaValue(root_path,tokens[1]);
+                    if(Utils::isFileType(translated_filename)){
+                        implCAT(translated_filename, tokens[1]);
+                    }
+                    break;
                 }
-                break;
-            }
 
-            case CMD_CAT: {
-                string root_path = Utils::getRootDirPath(implPWD(), FILE_SYSTEM);
-                string translated_filename = Randomizer::getMetaValue(root_path,tokens[1]);
-                if(Utils::isFileType(translated_filename)){
-                    implCAT(translated_filename, tokens[1]);
+                case CMD_LS: {
+                    implLS();
+                    break;
                 }
-                break;
-            }
 
-            case CMD_LS: {
-                implLS();
-                break;
-            }
+                case CMD_CD: {
+                    implCD(tokens[1]);
+                    break;
+                }
 
-            case CMD_CD: {
-                implCD(tokens[1]);
-                break;
-            }
+                case CMD_PWD: {
+                    cout << getRelativePWDPath() << endl;
+                    break;
+                }
 
-            case CMD_PWD: {
-                cout << getRelativePWDPath() << endl;
-                break;
-            }
+                case CMD_HELP: {
+                    displayMenu();
+                    break;
+                }
 
-            case CMD_HELP: {
-                displayMenu();
-                break;
-            }
-
-            default:
-                cout << "\nInvalid Command\n" << endl;
-                break; 
+                default:
+                    cout << "\nInvalid Command\n" << endl;
+                    break;
             }
         }
         return 0;
 
     }
+
     string getRelativePWDPath(){
         string pwd_path = implPWD();
         string root_dir = Utils::getRootDirPath(Utils::getPwdPath(),FILE_SYSTEM);
         string translated_path = Randomizer::getTranslatedPathRev(root_dir, pwd_path);
         return showPWD(translated_path);
     }
-
+    // 
     bool checkUsernameExist(string share_username = ""){
         string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
         string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
@@ -210,8 +211,7 @@ public:
         }
         return is_username_present;
     }
-
-
+    // 
     bool checkFilePresentInCurrentDir(string filename = "", string share_username = "") {
         string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
         string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
@@ -242,13 +242,11 @@ public:
         }
         return is_file_present;
     }
-
-
     void implShare(string filename, string share_username){
         string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
         string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
         string share_destination_path = root_path + "/" + share_username + "/"
-                + Randomizer::getMetaValue(root_path, SHARED);
+                                        + Randomizer::getMetaValue(root_path, SHARED);
         string cmd = CP + " " + filename + " " + share_destination_path;
         int exit_code = system(cmd.c_str());
         if (exit_code == 0) {
@@ -257,18 +255,6 @@ public:
             cout << "Failed to share the file." << endl;
         }
     }
-
-    string implPWD() {
-        string cwd = Utils::getPwdPath();
-        vector<string> tokens = Utils::split(cwd, '/');
-
-        string pwd = cwd + "/" + FILE_SYSTEM;
-        string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
-
-        current_dir_name = Randomizer::getMetaKey(root_path, tokens[tokens.size() - 1]);
-        return cwd;
-    }
-    
     string showPWD(string pwd_path){
         if(pwd_path.empty()) return"";
         vector<string> pwd_path_parts = Utils::split(pwd_path, '/');
@@ -281,7 +267,46 @@ public:
         string abs_pwd_path = Utils::joinVectorToString(pwd_path_parts, index+1, "/");
         return "/" + abs_pwd_path;
     }
+    // 
+    int createUser(string root_dir_path, string username) {
+        Utils::translateDirOrFileWhenCreated(USER + username);
+        string randomized_name = Utils::translateDirOrFileWhenCreated(username);
+        string user_folder_path = root_dir_path + "/" + randomized_name;
+        int p_status = stat(user_folder_path.c_str(), &info);
+        if (p_status == 0 && S_ISDIR(info.st_mode)) {
+            cout << "User " << username << " already exists" << endl;
+            return 200;
+        }
+        int _status = mkdir(user_folder_path.c_str(), 0777);
+        if (_status == 0) {
+            string child_folder_local = user_folder_path + "/" + Randomizer::getMetaValue(root_dir_path, PERSONAL);
+            string child_folder_shared = user_folder_path + "/" + Randomizer::getMetaValue(root_dir_path, SHARED);
+            mkdir(child_folder_local.c_str(), 0777);
+            mkdir(child_folder_shared.c_str(), 0777);
+            return 201;
+        }
+        else return 500;
+    }
+    // 
+    void implMKFILE(string filename, string content) {
+        string filename_value = Utils::translateDirOrFileWhenCreated(filename);
+        string public_key_path = Utils::getPublicUserKeys() + "/" + username + PUBLIC_KEY_EXT;
+        mRSAEncryption.encryptRSA(content, public_key_path, filename_value);
+    }
+    // 
 
+    // 
+    string implPWD() {
+        string cwd = Utils::getPwdPath();
+        vector<string> tokens = Utils::split(cwd, '/');
+
+        string pwd = cwd + "/" + FILE_SYSTEM;
+        string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
+
+        current_dir_name = Randomizer::getMetaKey(root_path, tokens[tokens.size() - 1]);
+        return cwd;
+    }
+    // 
     void implCAT(string file_name, string actual_filename) {
         string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
         string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
@@ -296,12 +321,12 @@ public:
             string _username = Utils::getUsernameFromFilePath(Utils::getPwdPath());
             target_username = Randomizer::getMetaKey(root_path,_username);
             private_key_name_for_admin = paths[0] + "/" +
-                                                Randomizer::getMetaValue(root_path, PRIVATE) + "/" +
-                                                target_username + PRIVATE_KEY_EXT;
+                                         Randomizer::getMetaValue(root_path, PRIVATE) + "/" +
+                                         target_username + PRIVATE_KEY_EXT;
         }
         mRSAEncryption.printDecryptedContent(file_name,  private_key_path, is_admin, private_key_name_for_admin, paths);
     }
-
+    // 
     void displayMenu() {
         cout << "\n";
         cout << "Welcome: " + username << endl;
@@ -324,7 +349,7 @@ public:
         }
         cout << "\n";
     }
-
+    // 
     void implLS() {
         string pwd = Utils::getPwdPath() + "/" + FILE_SYSTEM;
         string root_path = Utils::getRootDirPath(pwd, FILE_SYSTEM);
@@ -355,8 +380,8 @@ public:
         closedir(dir);
     }
 
-    
-     void implCD(string token) {
+    // 
+    void implCD(string token) {
         string translated_path;
         string root_dir = Utils::getRootDirPath(Utils::getPwdPath(),FILE_SYSTEM);
         translated_path = Randomizer::getTranslatedPath(root_dir, token);
@@ -371,6 +396,7 @@ public:
         chdir(translated_path.c_str());
     }
 
+    // 
     string getFuturePath(string file_path) {
         try {
             string command = "cd " + file_path + "&& pwd";
@@ -397,33 +423,6 @@ public:
             return "";
         }
     };
-
-    int createUser(string root_dir_path, string username) {
-        Utils::translateDirOrFileWhenCreated(USER + username);
-        string randomized_name = Utils::translateDirOrFileWhenCreated(username);
-        string user_folder_path = root_dir_path + "/" + randomized_name;
-        int p_status = stat(user_folder_path.c_str(), &info);
-        if (p_status == 0 && S_ISDIR(info.st_mode)) {
-            cout << "User " << username << " already exists" << endl;
-            return 200;
-        }
-        int _status = mkdir(user_folder_path.c_str(), 0777);
-        if (_status == 0) {
-            string child_folder_local = user_folder_path + "/" + Randomizer::getMetaValue(root_dir_path, PERSONAL);
-            string child_folder_shared = user_folder_path + "/" + Randomizer::getMetaValue(root_dir_path, SHARED);
-            mkdir(child_folder_local.c_str(), 0777);
-            mkdir(child_folder_shared.c_str(), 0777);
-            return 201;
-        }
-        else return 500;
-    }
-
-    void implMKFILE(string filename, string content) {
-        string filename_value = Utils::translateDirOrFileWhenCreated(filename);
-        string public_key_path = Utils::getPublicUserKeys() + "/" + username + PUBLIC_KEY_EXT;
-        mRSAEncryption.encryptRSA(content, public_key_path, filename_value);
-    }
-
 };
 
 #endif //FILESYSTEM_APP_MENU_H
